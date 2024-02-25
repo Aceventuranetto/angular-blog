740ecc9e0c47b620b0fd5fe761529b3cbf86d13f
Alexsander <aceventuranetto@gmail.com>
2024-02-25 13:33:06 -0300

    add images and text in the datafake

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -8,8 +8,23 @@ export const dataFake = [
   {
     "id":"2",
     "title":"Nova série anunciada no Disney +",
-    "description": "bla bla bla",
+    "description": "O primeiro lançamento da Marvel em 2024 foi logo no início do ano. A série “Eco”, sobre a antagonista de “Gavião Arqueiro” (2021), chegou ao Disney+ em 10 de janeiro.",
     "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
   },
 
+  {
+    "id":"3",
+    "title":"Novo filme do Pantera negra lançado em breve!",
+    "description": "O Marvel Studios vai expandir o universo de Pantera Negra com uma série de animação para o Disney+, intitulada Eyes of Wakanda. A produção foi anunciada em evento de pré-estreia da 2ª temporada de What If...? e repercutida pelo ComicBook. Eyes of Wakanda estreia em 2024 no streaming.",
+    "photoCover":"https://cdn.ome.lt/u1OTBhMwUe0_lIwqGCiXSvk1CMU=/570x0/smart/uploads/conteudo/fotos/wakanda_eEJv0wk.jpg"
+  },
+
+  {
+    "id":"4",
+    "title":"Quando o próximo filme dos X-Men no MCU será lançado?",
+    "description": "Os fãs dos mutantes mais famosos do mundo finalmente têm razões para comemorar, pois os X-Men estão prestes a fazer sua tão esperada estreia no Universo Cinematográfico da Marvel (MCU). Após anos de especulação e ansiedade, a Disney, que adquiriu os direitos dos X-Men da 20th Century Fox, está preparando o terreno para uma nova era dos seres superpoderosos nas telonas.",
+    "photoCover":"https://tm.ibxk.com.br/2023/10/06/06153113366326.jpg?ims=704x264"
+  },
+
 ];
+
